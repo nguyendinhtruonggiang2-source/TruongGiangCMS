10b3f9eaa4654d12ee67eb22f9103d88056252f6
GiangNguyen <nguyendinhtruonggiang2@gmail.com>
2026-05-29 15:23:50 +0700

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO Categories(Name, Description)
+VALUES
+('Tin Công Nghệ', 'Review Laptop, AI'),
+('Giáo Dục', 'Thông tin tuyển sinh');
+INSERT INTO Categories(Name, Description)
+VALUES ('Game', 'Tin game mới');
+
+SELECT * FROM Categories
